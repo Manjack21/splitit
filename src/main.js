544ab8c14a564512ad72710b998bbae5e37b21d9
Marco Winter <marco.winter@the-winters.de>
2021-07-02 17:03:49 +0200

    removed unnecessary function

--- src/main.js
+++ src/main.js
@@ -8,20 +8,6 @@ Vue.config.productionTip = false
 Vue.prototype.$lang = new Language("de");
 Vue.component('i18n', I18n);
 
-Vue.filter('currencyDisplay', {
-  // model -> view
-  // formats the value when updating the input element.
-  read: function(val) {
-    return val.toFixed(2) + ' €'
-  },
-  // view -> model
-  // formats the value when writing to the data.
-  write: function(val) {
-    var number = +val.replace(',', '.').replace(/[^\d.]/g, '')
-    return isNaN(number) ? 0 : parseFloat(number.toFixed(2))
-  }
-})
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
